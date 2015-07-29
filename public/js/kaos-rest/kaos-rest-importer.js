3ff933b9e74866b0efa4821ff469d13cce9c1c95
Michael Vignati <mvignati@ihmc.us>
2015-07-29 15:05:57 -0500

    Added HTML markup for import button Added new class in charge of conneting to KAoS rest api and loading the activity graph Fixed setTranslation to set the center of the node to the desired location Readded snapping for node Change private methods for calculating centers to use new translation Added function that generates activity graph from json Fixed and refactored goal related functions Added a function in charge of creating edges

---
+++ public/js/kaos-rest/kaos-rest-importer.js
@@ -0,0 +1,23 @@
+const URL = 'http://localhost:8080/kaos/kpl/getActivityGraph';
+
+export default class KAoSRestImporter {
+	constructor() {
+		let mHeaders = new Headers();
+		mHeaders.append('Accept', 'application/json');
+		mHeaders.append('Content-Type', 'application/json');
+
+		this._init = {
+			method: "GET",
+			headers: mHeaders,
+			mode: "cors"
+		};
+
+	}
+
+	getActivityGraph() {
+		let request = new Request(URL, this._init);
+
+		return fetch(request)
+		.then(response => response.json())
+	}
+}
