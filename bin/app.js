58d86cd8c9a5f182658342cf625e325aaa74e1b0
Michael Vignati <mvignati@ihmc.us>
2016-12-12 13:03:06 -0600

    Fixed issue with new handler format.

--- bin/app.js
+++ bin/app.js
@@ -1,6 +1,16 @@
 const Server = require('goblin/lib/server.js');
+const FileHandler = require('goblin/lib/file-handler.js');
+const RollupHandler = require('goblin/lib/rollup-handler.js');
+const ROLLUP_OPTIONS = {
+	include_paths: {
+		include: {},
+		paths: ['./node_modules/'],
+		external: [],
+		extensions: ['.js']
+	}
+};
 
 const server = new Server(process.argv[2], process.argv[3]);
-server.addRoute('*', Server.FILE_HANDLER);
-server.addRoute('^.*.js\\+bundle$', Server.ROLLUP_HANDLER);
+server.addRoute('*', new FileHandler());
+server.addRoute('^.*.js\\+bundle$', new RollupHandler(ROLLUP_OPTIONS));
 server.start();
