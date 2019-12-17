822210acbae59de0cf92bccbe81344a58c4471f0
Carson Wilber <cwilber@ihmc.us>
2019-12-17 13:46:56 -0600

    Remove rollup dependency.

--- bin/app.js
+++ bin/app.js
@@ -1,16 +1,6 @@
-const Server = require('goblin/lib/server.js');
+const Server = require('goblin/lib/server.js').Server;
 const FileHandler = require('goblin/lib/file-handler.js');
-const RollupHandler = require('goblin/lib/rollup-handler.js');
-const ROLLUP_OPTIONS = {
-	include_paths: {
-		include: {},
-		paths: ['./node_modules/'],
-		external: [],
-		extensions: ['.js']
-	}
-};
 
 const server = new Server(process.argv[2], process.argv[3]);
 server.addRoute('*', new FileHandler());
-server.addRoute('^.*.js\\+bundle$', new RollupHandler(ROLLUP_OPTIONS));
 server.start();
