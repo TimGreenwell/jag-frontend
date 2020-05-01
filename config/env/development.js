d59f75a1c92433faa4a7a8acf589dfa791e49bb2
Carson Wilber <cwilber@ihmc.us>
2020-05-01 14:02:15 -0500

    Reintroduce SSL environments.

---
+++ config/env/development.js
@@ -0,0 +1,4 @@
+module.exports = function (app) {
+    // Implement any functionalities or configuration specific to devleopment environment.
+    app.set('port', 8888);
+};
---
+++ config/env/production.js
@@ -0,0 +1,4 @@
+module.exports = function (app) {
+    // Implement any functionalities or configuration specific to production environment.
+    app.set('port', 8889);
+};
