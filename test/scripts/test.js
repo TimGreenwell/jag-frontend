248c8408b495da040121e3884e573f112c9b028d
Carson Wilber <cwilber@ihmc.us>
2020-07-20 23:10:25 -0500

    First draft testing storage types.

--- test/scripts/test.js
+++ test/scripts/test.js
@@ -3,84 +3,164 @@
  *
  * @author mvignati
  * @copyright Copyright © 2020 IHMC, all rights reserved.
- * @version 0.89
+ * @version 1.15
  */
 import '/scripts/mocha.js';
 import JAG from '/src-scripts/models/jag.js';
 import JAGService from '/src-scripts/services/jag.js';
 import IndexedDBStorage from '/src-scripts/storages/indexed-db.js';
+import BasicMapStorage from '/scripts/map-storage.js';
 
 mocha.setup('tdd');
 
-suite('JAG Service with IndexedDB storage', async () => {
-
-	console.log('Initializing test indexed db storage storage');
-	const idb_storage = new IndexedDBStorage('joint-activity-graphs-test', 1);
-	await idb_storage.init();
-	const idb_service = JAGService.createInstance('idb-service', idb_storage);
-
-	suite('POST /jags/', () => {
-
-		const test_jag = new JAG({
-			urn: 'urn:ihmc:web-tools:tests:test-jag',
-			name: 'A test jag',
-			description: 'A test jag description',
-		});
-
-		test('should store a jag', async () => {
-			await idb_service.create(test_jag);
-		});
-
-		test('should fail to store an existing jag', async () => {
-		});
-
-	});
-
-	suite('GET /jags/{id}', () => {
-
-		test('Should return jag with the specified id.', async () => {
-			const urn = 'urn:ihmc:web-tools:tests:test-jag';
-			const jag = await idb_service.get(urn);
-			if(jag.urn !== urn) throw new Error();
-		});
-
-		test('Should return null for a jag that does not exist.', async () => {
-			const urn = 'urn:ihmc:web-tools:tests:does-not-exist';
-			const jag = await idb_service.get(urn);
-			if(jag !== null) throw new Error();
-		});
-
-	});
-
-	suite('GET /jags', () => {
-
-		test('should return all jags', async () => {
-			const jags = await idb_service.all();
-			console.log(jags);
-		});
-
-	})
-
-	suite('HEAD /jags/{id}', () => {
-
-		test('should return true for an existing key', async () => {
-			const exists = await idb_service.has('urn:ihmc:web-tools:tests:test-jag');
-			if(!exists) throw new Error();
-
-		});
-
-		test('should return false for a non existing key', async () => {
-			const exists = await idb_service.has('urn:ihmc:web-tools:tests:does-not-exists');
-			if(exists) throw new Error();
-		});
-
-	})
-
-	suiteTeardown(async () => {
-		// @TODO: remove testing database
-		console.log('Cleaning out indexed db storage');
-	});
+const test_jag_a = new JAG({
+	urn: 'urn:ihmc:web-tools:tests:test-jag-alpha',
+	name: 'Alpha test JAG',
+	description: 'Alpha test JAG description',
 });
+
+const test_jag_b = new JAG({
+	urn: 'urn:ihmc:web-tools:tests:test-jag-beta',
+	name: 'Beta test JAG',
+	description: 'Beta test JAG description',
+});
+
+const unknown_urn = 'urn:ihmc:web-tools:tests:test-jag-gamma';
+
+const storages = [
+	{
+		'id': 'map',
+		'name': 'BasicMapStorage',
+		'instance': new BasicMapStorage()
+	},
+	{
+		'id': 'idb',
+		'name': 'IndexedDBStorage',
+		'instance': new IndexedDBStorage('joint-activity-graphs-test', 1)
+	}
+];
+
+storages.forEach(
+	(storage) => suite(`Test storage ${storage.name}`, async () => {
+		await storage.instance.init();
+		const service = JAGService.createInstance(`${storage.id}-service`, storage.instance);
+
+		suite(`Create JAG in ${storage.name}`, () => {
+
+			test('should create a new JAG', async () => {
+				try {
+					await service.create(test_jag_a);
+					await service.create(test_jag_b);
+				} catch (e) {
+					throw new Error(`Failed to create JAG ${e.message}.`);
+				}
+			});
+
+			test('should fail to create an existing JAG', async () => {
+				try {
+					await service.create(test_jag_a);
+
+					throw new Error(`Should not have created JAG with already defined URN.`);
+				} catch {
+					// Success; should have thrown an error
+				}
+			});
+
+		});
+
+		suite(`Retrieve JAG from ${storage.name}`, () => {
+
+			test('should return JAG with the specified URN.', async () => {
+				try {
+					await service.get(test_jag_a.urn);
+				} catch (e) {
+					throw new Error(`Failed to retrieve JAG: ${e.message}.`);
+				}
+
+				// TODO: deep equality check of returned JAG
+			});
+
+			test('should return null for a JAG that does not exist.', async () => {
+				try {
+					await service.get(unknown_urn);
+
+					throw new Error(`Should not have retrieved JAG with unknown URN.`);
+				} catch {
+					// Success; should not have thrown error
+				}
+			});
+
+		});
+
+		suite(`List available JAGs in ${storage.name}`, () => {
+
+			test('should return all JAGs', async () => {
+				let jag_list;
+
+				try {
+					jag_list = await service.all();
+				} catch {
+					throw new Error(`Failed to list available JAGs.`);
+				}
+
+				if (jag_list.length < 2)
+					throw new Error(`List has too few JAGs (expected 2, retrieved ${jag_list.length}).`);
+
+				if (jag_list.length > 2)
+					throw new Error(`List has too many JAGs (expected 2, retrieved ${jag_list.length}).`);
+
+				const jag_list_urns = jag_list.map(jag => jag.urn);
+
+				// TODO: deep equality check?
+				if (!jag_list_urns.includes(test_jag_a.urn))
+					throw new Error(`List does not contain an expected JAG.`);
+
+				// TODO: deep equality check?
+				if (!jag_list_urns.includes(test_jag_b.urn))
+					throw new Error(`List contains an unexpected JAG.`);
+
+				// NOTE: This indirectly affirms no unexpected JAGs are included in the list,
+				//       thus no additional test is needed.
+			});
+
+		});
+
+		suite(`Check for existence of JAG in ${storage.name}`, () => {
+
+			test('should return true for an existing key', async () => {
+				let a_exists;
+
+				try {
+					a_exists = await service.has(test_jag_a.urn);
+				} catch {
+					throw new Error(`Failed to check for existence of existing JAG.`);
+				}
+
+				if (a_exists !== true)
+					throw new Error(`Check for existing JAG did not return true.`);
+			});
+
+			test('should return false for a non existing key', async () => {
+
+				let unknown_exists;
+
+				try {
+					unknown_exists = await service.has(unknown_urn);
+				} catch {
+					throw new Error(`Failed to check for existence of nonexisting JAG.`);
+				}
+
+				if (unknown_exists !== false)
+					throw new Error(`Check for nonexisting JAG did not return false.`);
+			});
+
+		});
+
+		suiteTeardown(async () => {
+			// TODO: cleanup storage/service
+		});
+	})
+);
 
 mocha.run();
 
